23796921905e321846d25ef285f9fec9dc125873
tamir <tamirpa123@gmail.com>
2020-01-14 17:37:24 +0200

    request fragment progress

--- app/src/main/java/il/ac/technion/socialmoneyexchange/RequestFragment.kt
+++ app/src/main/java/il/ac/technion/socialmoneyexchange/RequestFragment.kt
@@ -12,13 +12,21 @@ import com.google.android.material.internal.ViewUtils.dpToPx
 import kotlinx.android.synthetic.main.fragment_request.view.*
 import android.text.InputFilter
 import android.view.inputmethod.EditorInfo
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
-
-
-
+import com.google.android.material.textview.MaterialTextView
+import com.google.gson.GsonBuilder
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.internal.schedulers.IoScheduler
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+//import rx.android.schedulers.AndroidSchedulers
 
 class RequestFragment : Fragment() {
-    private val MAX_CURRNECIES = 5F
+    private val MAX_CURRENECIES = 5F
+    lateinit var inputText : TextInputEditText
     @SuppressLint("SetTextI18n", "RestrictedApi")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -32,19 +40,31 @@ class RequestFragment : Fragment() {
         coinList.add("five")
         coinList.add("one")
 
+
+        //internet
+
+//        val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//            .baseUrl("https://api.exchangeratesapi.io/").build()
+//        val ratesApi = retrofit.create(RatesAPI::class.java)
+//        var response = ratesApi.getRates()
+//        response.observeOn(IoScheduler()).subscribeOn(AndroidSchedulers.mainThread()).subscribe(
+//
+//        )
+
         var myAddedCoins = 0F
         val view = inflater.inflate(R.layout.fragment_request, container, false)
         val layout = view.request_layout as RelativeLayout
-        val spinnerList = ArrayList<com.toptoche.searchablespinnerlibrary.SearchableSpinner>()
-        val inputTextList = ArrayList<com.google.android.material.textview.MaterialTextView>()
+        val spinnerList = ArrayList<SearchableSpinner>()
+        val inputTextList = ArrayList<MaterialTextView>()
         val pickedCoin = ArrayList<Currency>()
-        for (i in 0..MAX_CURRNECIES.toInt()) {
+        for (i in 0..MAX_CURRENECIES.toInt()) {
             val currency = Currency("temp", 0)
             pickedCoin.add(currency)
         }
 
         //first spinner - giving currency
-        val spinner = com.toptoche.searchablespinnerlibrary.SearchableSpinner(requireContext())
+        val spinner = SearchableSpinner(requireContext())
         spinner.layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT)
         spinner.adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1,
@@ -65,9 +85,33 @@ class RequestFragment : Fragment() {
 
                 }
             })
-        //spinnerList.add(spinner)
         layout.addView(spinner)
-        val myButtonAdd = com.google.android.material.button.MaterialButton(requireContext())
+
+        //input text for giving
+        inputText = TextInputEditText(requireContext())
+        inputText.layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        val inputTextParam = inputText.layoutParams as RelativeLayout.LayoutParams
+        val inputTextEdgeDist = dpToPx(requireContext(),130).toInt()
+        val inputTextTopDist = dpToPx(requireContext(),70).toInt()
+        inputTextParam.topMargin = inputTextTopDist
+        inputTextParam.marginStart = inputTextEdgeDist
+        inputText.layoutParams = inputTextParam
+        inputText.hint="Insert amount"
+        inputText.inputType = InputType.TYPE_CLASS_NUMBER
+        inputText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(6))
+        inputText.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                for(i in 0 until myAddedCoins.toInt()){
+                    inputTextList[i].text = inputText.text
+                }
+                true
+            } else {
+                false
+            }
+        }
+        layout.addView(inputText)
+
+        val myButtonAdd = MaterialButton(requireContext())
         myButtonAdd.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         val addParam = myButtonAdd.layoutParams as RelativeLayout.LayoutParams
         val addEdgeDist = dpToPx(requireContext(),12).toInt()
@@ -78,7 +122,7 @@ class RequestFragment : Fragment() {
         myButtonAdd.text = "Add"
         layout.addView(myButtonAdd)
 
-        val myButtonRemove = com.google.android.material.button.MaterialButton(requireContext())
+        val myButtonRemove = MaterialButton(requireContext())
         myButtonRemove.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         val removeParam = myButtonRemove.layoutParams as RelativeLayout.LayoutParams
         val removeEdgeDist = dpToPx(requireContext(),250).toInt()
@@ -113,39 +157,19 @@ class RequestFragment : Fragment() {
             for(i in 0 until myAddedCoins.toInt()){
                 pickedString = pickedString + " " + pickedCoin[i].type + " " + pickedCoin[i].amount
             }
-            Toast.makeText(requireContext(),pickedString, Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(requireContext(),pickedString+" "+inputText.text, Toast.LENGTH_SHORT).show()
         }
 
         return view
     }
     @SuppressLint("RestrictedApi")
-    private fun addButtonClicked(spinnerList:ArrayList<com.toptoche.searchablespinnerlibrary.SearchableSpinner>, inputTextList:ArrayList<com.google.android.material.textview.MaterialTextView>,
+    private fun addButtonClicked(spinnerList:ArrayList<SearchableSpinner>, inputTextList:ArrayList<MaterialTextView>,
                                  addedCoins:Float, coinList:ArrayList<String>, layout:RelativeLayout, removeButton:Button, pickedCoin:ArrayList<Currency>):Boolean{
-        if(addedCoins < MAX_CURRNECIES) {
+        if(addedCoins < MAX_CURRENECIES) {
             var tempCoins = addedCoins
-//            val inputText = TextInputEditText(requireContext())
-//            inputText.layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
-//            val inputTextParam = inputText.layoutParams as RelativeLayout.LayoutParams
-//            val inputTextEdgeDist = dpToPx(requireContext(),130).toInt()
-//            val inputTextTopDist = dpToPx(requireContext(),110+24*tempCoins.toInt()).toInt()
-//            inputTextParam.topMargin = inputTextTopDist
-//            inputTextParam.marginStart = inputTextEdgeDist
-//            inputText.layoutParams = inputTextParam
-//            inputText.hint="Insert amount"
-//            inputText.inputType = InputType.TYPE_CLASS_NUMBER
-//            inputText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(6))
-//            inputText.setOnEditorActionListener { v, actionId, event ->
-//                if(actionId == EditorInfo.IME_ACTION_DONE){
-//                    pickedCoin[tempCoins.toInt()].amount=actionId
-//                    true
-//                } else {
-//                    false
-//                }
-//            }
-//            inputText.setOnClickListener({
-//
-//            })
-            val amountText = com.google.android.material.textview.MaterialTextView(requireContext())
+
+            val amountText = MaterialTextView(requireContext())
             amountText.layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
             val amountTextParam = amountText.layoutParams as RelativeLayout.LayoutParams
             val amountTextEdgeDist = dpToPx(requireContext(),130).toInt()
@@ -153,23 +177,9 @@ class RequestFragment : Fragment() {
             amountTextParam.topMargin = amountTextTopDist
             amountTextParam.marginStart = amountTextEdgeDist
             amountText.layoutParams = amountTextParam
-            amountText.text = "111"
-//            amountText.hint="Insert amount"
-//            amountText.inputType = InputType.TYPE_CLASS_NUMBER
-//            amountText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(6))
-//            amountText.setOnEditorActionListener { v, actionId, event ->
-//                if(actionId == EditorInfo.IME_ACTION_DONE){
-//                    pickedCoin[tempCoins.toInt()].amount=actionId
-//                    true
-//                } else {
-//                    false
-//                }
-//            }
-//            amountText.setOnClickListener({
-//
-//            })
+
             val spinner =
-                com.toptoche.searchablespinnerlibrary.SearchableSpinner(requireContext())
+                SearchableSpinner(requireContext())
             spinner.layoutParams = RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
@@ -189,6 +199,7 @@ class RequestFragment : Fragment() {
             val spinnerTopDist = dpToPx(requireContext(),370+24*tempCoins.toInt()).toInt()
             spinnerParam.topMargin = spinnerTopDist
             spinnerParam.marginStart = spinnerEdgeDist
+//            spinnerParam.marginEnd = dpToPx(requireContext(),0).toInt()
             spinner.layoutParams = spinnerParam
                         spinner.onItemSelectedListener =
                 (object : AdapterView.OnItemSelectedListener {
@@ -201,15 +212,17 @@ class RequestFragment : Fragment() {
                     ) {
                         val tempString = coinList[position]
                         pickedCoin[tempCoins.toInt()].type=tempString
-                        //Toast.makeText(requireContext(),tempCoins.toString(),Toast.LENGTH_SHORT).show()
 
                     }
                 })
+
             spinnerList.add(spinner)
             layout.addView(spinner)
             inputTextList.add(amountText)
             layout.addView(amountText)
-//            Toast.makeText(requireContext(), "Added ", Toast.LENGTH_SHORT).show()
+            for(i in 0 until addedCoins.toInt()+1){
+                inputTextList[i].text = inputText.text
+            }
             return true
         }
         else{

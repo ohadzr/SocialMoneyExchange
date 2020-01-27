3b1934f22e18325e72d06b306ee56208abba597d
Ohad <ohadzr@gmail.com>
2020-01-27 11:04:58 +0200

    Logo pop back stack

--- app/src/main/java/il/ac/technion/socialmoneyexchange/LogoFragment.kt
+++ app/src/main/java/il/ac/technion/socialmoneyexchange/LogoFragment.kt
@@ -1,15 +1,23 @@
 package il.ac.technion.socialmoneyexchange
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 
 class LogoFragment : Fragment() {
@@ -35,12 +43,25 @@ class LogoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var a = 10
-        //FIXME: change to pulling instead of while
-//        while (FirebaseAuth.getInstance().currentUser == null) {
-//            a = 5
-//        }
-//        findNavController().popBackStack()
-
+        waitForUserLogin()
     }
+
+
+    private fun waitForUserLogin() {
+        var stop = false
+
+        val mainHandler = Handler(Looper.getMainLooper())
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    stop = true
+                    findNavController().popBackStack()
+                }
+                if (!stop){
+                    mainHandler.postDelayed(this, 2000)
+                }
+            }
+        })
+    }
+
 }
